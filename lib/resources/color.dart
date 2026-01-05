3fa73136b3a03167412abf5f58f14807038b471e
IrtazaDev <irtazakhalidll@gmail.com>
2026-01-05 12:13:11 +0500

    color setup

---
+++ lib/resources/color.dart
@@ -0,0 +1,12 @@
+
+import 'dart:ui';
+
+class AppColors{
+
+static const Color primaryColor = Color(0xff896CFE);
+static const Color secondaryColor = Color(0xffB3A0FF);
+static const Color greenColor = Color(0xffE2F163);
+
+
+
+}
