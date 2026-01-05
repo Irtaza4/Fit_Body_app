5d750f835a17a00e5714981099d17b478adab588
IrtazaDev <irtazakhalidll@gmail.com>
2026-01-05 13:12:57 +0500

    router setup and splash screen

---
+++ lib/resources/images.dart
@@ -0,0 +1,6 @@
+
+class Images {
+
+  static const String appIcon = 'assets/icons/logo.svg';
+  static const String fitBody = 'assets/icons/FITBODY.svg';
+}
--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,15 +1,61 @@
+import 'package:fitness_app/resources/images.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<Offset> _fromLeft;
+  late final Animation<Offset> _fromRight;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this,
+    duration: Duration(milliseconds: 1200),
+    );
+
+    _fromLeft = Tween<Offset>(
+    begin: const Offset(-1.0, 0.0),
+    end: Offset.zero  
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+
+    _fromRight =Tween<Offset>(
+      begin: const Offset(1.0, 0.0),
+      end: Offset.zero
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+    _controller.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          
-        ],
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Column(children: [
+                SlideTransition(
+                    position: _fromLeft,
+                    child: SvgPicture.asset(Images.appIcon)),
+                const SizedBox(height: 20),
+                SlideTransition(
+                    position: _fromRight,
+                    child: SvgPicture.asset(Images.fitBody)),
+              ],),
+            ),
+        
+          ],
+        ),
       ),
     );
   }
